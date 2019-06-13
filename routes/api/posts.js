bc78ec9f38156ccf21b4776795a00d8f44d58f94
Bhanuj <bhabawdh>
2019-06-13 10:17:07 +0530

    Fixed the middleware Auth typo

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
-const auth = require('../../middleware/auth');
+const auth = require('../../middleware/Auth');
 
 const Post = require('../../models/Post');
 const User = require('../../models/User');
