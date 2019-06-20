ca375f9ca681f1be462e2d29a0c33384667d219c
Bhanuj <bhabawdh>
2019-06-20 16:39:31 +0530

    Added JWT/Mongo Credentials for Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/db');
 const path = require('path');
 
 // Adding Redis Cache Impl to cache the Query results returned by MongoDB.
-require('./hooks/RedisCache');
+// require('./hooks/RedisCache');
 
 const port = process.env.PORT || 5000;
 const app = express();
